086c227a294a9eec9446e6669d9a5b432ebfe642
Omm Mishra <ommmishra830@gmail.com>
2019-01-22 21:26:34 +0530

    changed some values

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from gevent.pywsgi import WSGIServer
 from random import randint
+from settings import RULES, BOT_INTRO
 import requests
 import os
 
@@ -9,15 +10,13 @@ app = Flask(__name__)
 TOKEN = os.environ.get('TOKEN')
 BASE_URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 GROUP_CHAT_ID = int(os.environ.get('group_chat_id'))
-RULES = "For the new people joining:\n\nThere's a desktop and web client for Telegram so that you can stay updated everywhere. \n\nWe keep track of all members:\n\nhttp://codex.subhrajitpy.me\n\nAdd your names:\nhttps://github.com/SubhrajitPrusty/codex-github/issues/17\n\nSome channels and groups that you can utilise on telegram.\n\n@linux_group\n@thedevs\n@pythongroup\n@python\n@science\n@theprogrammingartgroup\n@TechGuide\n@fullstackbroadcast\n@linuxgram\n@techbinder\n@javaebooks\n@theprogrammingart\n@letstalkprogramming\n\nSome useful bots:\n@rextester_bot\n@gif\n@stickers\n@wiki\n@imdb\n\nBe respectful. Dont spam. Read the group rules.\n\nIn the @codexinit group \n\n* check someone's submission - ask them if they can make improvements \n\n* ask them what they're familiar with or working on\n\n* give them a suitable minor - but something that they have to learn in a short time period\nor if they seem weak with structuring code, then something that requires a good structure (a game would be one example)\n\nYou don't have to be an admin to review submissions."
-BOT_INTRO =  "Hello I'm Alfred the official butler of CODEX, here are somethings I can do:\n\n/helpme for Alfred's commands\n/xkcd for a bit of everthing\n/rules for CODEXs' rules"
 
 @app.route("/update", methods = ["POST"])
 def update():
 	print("RunningBot.......")
 	print(request.get_json())
 	data = request.get_json()
-	group_data = int(data['message']['chat']['id'])
+	group_data = int(data.get('message').get('chat').get('id'))
 
 	if group_data == GROUP_CHAT_ID:
 
